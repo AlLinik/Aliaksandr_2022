14298242201323ca3880f052915e08f9e2f80bb4
HP <terlin@mail.ru>
2022-08-15 00:06:48 +0300

    start_15_08_2022

--- aliaksandr_2022/blog/views.py
+++ aliaksandr_2022/blog/views.py
@@ -3,8 +3,16 @@ from .models import Post
 
 
 def blog_index(request):
-    posts = Post.objects.all()
+    posts = Post.objects.all().order_by('-created_on')
     context = {
-        'posts': posts
+        'posts': posts,
     }
     return render(request, 'blog_index.html', context)
+
+
+def blog_detail(request, pk):
+    post = Post.objects.get(pk=pk)
+    context = {
+        'post': post,
+    }
+    return render(request, 'blog_detail.html', context)
